032399ee4d932e87d96554b3d50aed6766644bf9
unknown <afridi.azeez07@gmail.com>
2024-02-29 17:02:44 +0530

    change 2

--- zms/admin/index.php
+++ zms/admin/index.php
@@ -24,7 +24,7 @@ if(isset($_POST['login']))
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Login - Zoo Management System</title>
+    <title>Login - The Zoo Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
